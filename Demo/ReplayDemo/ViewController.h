99719b533c92142e425f60fd004b72cfc283b0a7
mysong <mysong@tencent.com>
2017-10-11 10:14:36 +0800

    new project

---
+++ Demo/ReplayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReplayDemo
+//
+//  Created by mysong on 2017/10/11.
+//  Copyright © 2017年 mys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Demo/ReplayDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReplayDemo
+//
+//  Created by mysong on 2017/10/11.
+//  Copyright © 2017年 mys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
